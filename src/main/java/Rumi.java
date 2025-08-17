0a1a138b7253e0c1e543bd92d2a0c9f327f881ca
Florian Viorel Tanely <izkx73@gmail.com>
2025-08-17 19:49:17 +0800

    Refactor logo initialisation and greetings into methods

--- src/main/java/Rumi.java
+++ src/main/java/Rumi.java
@@ -5,32 +5,28 @@ import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 public class Rumi {
-    public static void main(String[] args) {
-        final String CHATBOT_NAME = "Rumi";
-        final String DIVIDER = "________________________________________________";
+    private static final String DIVIDER = "________________________________________________";
+    public static final String CHATBOT_NAME = "Rumi";
+    public static final String FALLBACK_LOGO = "\n" +
+        "\n" +
+        "                                ____           \n" +
+        "                              ,'  , `.  ,--,   \n" +
+        "  __  ,-.         ,--,     ,-+-,.' _ |,--.'|   \n" +
+        ",' ,'/ /|       ,'_ /|  ,-+-. ;   , |||  |,    \n" +
+        "'  | |' |  .--. |  | : ,--.'|'   |  ||`--'_    \n" +
+        "|  |   ,','_ /| :  . ||   |  ,', |  |,,' ,'|   \n" +
+        "'  :  /  |  ' | |  . .|   | /  | |--' '  | |   \n" +
+        "|  | '   |  | ' |  | ||   : |  | ,    |  | :   \n" +
+        ";  : |   :  | : ;  ; ||   : |  |/     '  : |__ \n" +
+        "|  , ;   '  :  `--'   \\   | |`-'      |  | '.'|\n" +
+        " ---'    :  ,      .-./   ;/          ;  :    ;\n" +
+        "          `--`----'   '---'           |  ,   / \n" +
+        "                                       ---`-'  \n" +
+        "\n";
 
-        String LOGO = null;
-        try {
-            LOGO = Files.readString(Paths.get("./src/logo.txt"), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            LOGO = "\n" +
-                    "\n" +
-                    "                                ____           \n" +
-                    "                              ,'  , `.  ,--,   \n" +
-                    "  __  ,-.         ,--,     ,-+-,.' _ |,--.'|   \n" +
-                    ",' ,'/ /|       ,'_ /|  ,-+-. ;   , |||  |,    \n" +
-                    "'  | |' |  .--. |  | : ,--.'|'   |  ||`--'_    \n" +
-                    "|  |   ,','_ /| :  . ||   |  ,', |  |,,' ,'|   \n" +
-                    "'  :  /  |  ' | |  . .|   | /  | |--' '  | |   \n" +
-                    "|  | '   |  | ' |  | ||   : |  | ,    |  | :   \n" +
-                    ";  : |   :  | : ;  ; ||   : |  |/     '  : |__ \n" +
-                    "|  , ;   '  :  `--'   \\   | |`-'      |  | '.'|\n" +
-                    " ---'    :  ,      .-./   ;/          ;  :    ;\n" +
-                    "          `--`----'   '---'           |  ,   / \n" +
-                    "                                       ---`-'  \n" +
-                    "\n";
-        }
+    public static String LOGO = null;
 
+    private static void showIntroduction() {
         System.out.println(LOGO);
         System.out.println(DIVIDER);
         System.out.printf("Welcome back, master. %s at your service (๑˃ᴗ˂)ﻭ!\n", CHATBOT_NAME);
@@ -38,5 +34,18 @@ public class Rumi {
         System.out.println(DIVIDER);
         System.out.println("Goodbye master. Have a great day ahead!");
         System.out.println(DIVIDER);
+    }
+
+    private static void initialiseLogo() {
+        try {
+            LOGO = Files.readString(Paths.get("./src/logo.txt"), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            LOGO = FALLBACK_LOGO;
+        }
+    }
+
+    public static void main(String[] args) {
+        initialiseLogo();
+        showIntroduction();
     }
 }
