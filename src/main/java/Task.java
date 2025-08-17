9fed214b824e8d16991d50dc161bdbc091e7136d
Florian Viorel Tanely <izkx73@gmail.com>
2025-08-17 21:43:25 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    private String title;
+    private boolean isDone;
+
+    public Task(String title) {
+        this.title = title;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.isDone ? 'X' : ' ', this.title);
+    }
+}
