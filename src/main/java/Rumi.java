d98cef281455fb2459e248b7b888c480b411cbcc
Florian Viorel Tanely <izkx73@gmail.com>
2025-08-17 19:34:31 +0800

    Add logo and greetings

--- src/main/java/Rumi.java
+++ src/main/java/Rumi.java
@@ -1,10 +1,42 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
 public class Rumi {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        final String CHATBOT_NAME = "Rumi";
+        final String DIVIDER = "________________________________________________";
+
+        String LOGO = null;
+        try {
+            LOGO = Files.readString(Paths.get("./src/logo.txt"), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            LOGO = "\n" +
+                    "\n" +
+                    "                                ____           \n" +
+                    "                              ,'  , `.  ,--,   \n" +
+                    "  __  ,-.         ,--,     ,-+-,.' _ |,--.'|   \n" +
+                    ",' ,'/ /|       ,'_ /|  ,-+-. ;   , |||  |,    \n" +
+                    "'  | |' |  .--. |  | : ,--.'|'   |  ||`--'_    \n" +
+                    "|  |   ,','_ /| :  . ||   |  ,', |  |,,' ,'|   \n" +
+                    "'  :  /  |  ' | |  . .|   | /  | |--' '  | |   \n" +
+                    "|  | '   |  | ' |  | ||   : |  | ,    |  | :   \n" +
+                    ";  : |   :  | : ;  ; ||   : |  |/     '  : |__ \n" +
+                    "|  , ;   '  :  `--'   \\   | |`-'      |  | '.'|\n" +
+                    " ---'    :  ,      .-./   ;/          ;  :    ;\n" +
+                    "          `--`----'   '---'           |  ,   / \n" +
+                    "                                       ---`-'  \n" +
+                    "\n";
+        }
+
+        System.out.println(LOGO);
+        System.out.println(DIVIDER);
+        System.out.printf("Welcome back, master. %s at your service (๑˃ᴗ˂)ﻭ!\n", CHATBOT_NAME);
+        System.out.println("What should I do?");
+        System.out.println(DIVIDER);
+        System.out.println("Goodbye master. Have a great day ahead!");
+        System.out.println(DIVIDER);
     }
 }
