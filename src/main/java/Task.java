72487f80792f24c92db1175b24a154f93272073c
Florian Viorel Tanely <izkx73@gmail.com>
2025-08-19 02:31:37 +0800

    Task class: override equals and hashCode methods

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,11 @@
+import java.util.Objects;
+
 public class Task {
     private String title;
     private boolean isDone;
 
     public Task(String title) {
+        assert title != null;
         this.title = title;
         this.isDone = false;
     }
@@ -18,5 +21,18 @@ public class Task {
     @Override
     public String toString() {
         return String.format("[%s] %s", this.isDone ? 'X' : ' ', this.title);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Task task)) return false;
+
+        return this.isDone == task.isDone && this.title.equals(task.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.title, this.isDone);
     }
 }
