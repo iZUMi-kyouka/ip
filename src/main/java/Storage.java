d2ccab4f5e24c3d75be0a2dc89838b0362c38717
Florian Viorel Tanely <izkx73@gmail.com>
2025-08-27 14:04:49 +0800

    Storage class: implement loadTasks

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -7,12 +7,37 @@ public class Storage {
     private final String FILENAME = ".rumi_data";
 
     public ArrayList<Task> loadTasks() {
+        ArrayList<Task> tasks = new ArrayList<>();
         try (Scanner sc = new Scanner(new File(".rumi_data"))) {
             while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+                String task = sc.nextLine();
+                if (task.isEmpty()) {
+                    continue;
+                }
+
+                try {
+                    switch (task.charAt(0)) {
+                        case 'E':
+                            tasks.add(Event.fromString(task));
+                            break;
+                        case 'D':
+                            tasks.add(Deadline.fromString(task));
+                            break;
+                        case 'T':
+                            tasks.add(ToDo.fromString(task));
+                            break;
+                        default:
+                            continue;
+                    }
+
+                } catch (IllegalArgumentException e) {
+                    System.out.printf("[WARN] %s\n", e);
+                }
             }
         } catch (FileNotFoundException e) {
             return new ArrayList<>();
         }
+
+        return tasks;
     }
 }
