d428483723f82596ddb7fd1c8765b079aaafee5b
Florian Viorel Tanely <izkx73@gmail.com>
2025-08-27 13:34:30 +0800

    ToDo class: implement fromString function

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,4 +1,9 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ToDo extends Task {
+    private final String SEPARATOR = " @|@ ";
+
     ToDo(String title) {
         super(title);
     }
@@ -6,5 +11,15 @@ public class ToDo extends Task {
     @Override
     public String toString() {
         return String.format("[T]%s", super.toString());
+    }
+
+    public static ToDo fromString(String s) {
+        Pattern pattern = Pattern.compile("T\\s+@|@\\s+(.+?)\\s+");
+        Matcher matcher = pattern.matcher(s);
+
+        if (matcher.matches()) {
+            String title = matcher.group(1);
+            return new ToDo(title);
+        }
     }
 }
