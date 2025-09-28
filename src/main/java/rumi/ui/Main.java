93e933b76ddb7e1e3849a0996a54d0bce49c513a
Florian Viorel Tanely <izkx73@gmail.com>
2025-09-28 14:59:11 +0800

    Fix custom fonts not not loaded

--- src/main/java/rumi/ui/Main.java
+++ src/main/java/rumi/ui/Main.java
@@ -1,12 +1,14 @@
 package rumi.ui;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import rumi.Rumi;
 
@@ -18,6 +20,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            loadFonts();
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
@@ -38,6 +41,33 @@ public class Main extends Application {
         } catch (IOException e) {
             e.printStackTrace();
             System.out.printf("[ERROR] Failed to initialise GUI: %s\n", e.getMessage());
+        }
+    }
+
+    private void loadFonts() {
+        // Load required fonts
+        InputStream robotoStream = Main.class.getResourceAsStream("/fonts/Roboto-Regular.ttf");
+        InputStream iosevkaStream = Main.class.getResourceAsStream("/fonts/Iosevka-Medium.ttf");
+
+        if (robotoStream == null) {
+            System.err.println("[ERROR] Font 'Roboto' not found.");
+        }
+        if (iosevkaStream == null) {
+            System.err.println("[ERROR] Font 'Iosevka Medium' not found.");
+        }
+
+        Font fontIosevka = Font.loadFont(iosevkaStream, 16);
+        Font fontRoboto = Font.loadFont(robotoStream, 16);
+        if (fontIosevka == null) {
+            System.err.println("[ERROR] Font 'Iosevka Medium' is corrupted.");
+        } else {
+            System.out.printf("[INFO] Using font 'Iosevka Medium' with family '%s'\n",
+                    fontIosevka.getFamily());
+        }
+        if (fontRoboto == null) {
+            System.err.println("[ERROR] Font 'Roboto' is corrupted.");
+        } else {
+            System.out.printf("[INFO] Using font 'Roboto' with family '%s'\n", fontRoboto.getFamily());
         }
     }
 }
